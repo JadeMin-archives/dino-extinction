74b5a79b8e08176fe284ce15259acb93b012de4d
JadeMin <36400787+JadeMin@users.noreply.github.com>
2021-07-12 16:38:02 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+/*function confirmReload(isOver){
+    var answer = isOver? confirm(true) :
+        confirm(false);
+    if(answer) location.reload();
+}*/
+
+if(!window.dinoBot) {
+    var dinoBot = Runner.instance_;
+    var endTime = 6,
+        solvedTime = endTime * 1000;
+    dinoBot.gameEnd = dinoBot.gameOver;
+    dinoBot.gameOver = function(){};
+    
+    dinoBot.canvas.focus();
+    dinoBot.playIntro();
+    dinoBot.startGame();
+    dinoBot.setSpeed(99999);
+    
+    setInterval(function(){
+        if(Runner.instance_.playCount > 1 && dinoBot.gameOver.toString() == "function(){}") {
+            Runner.instance_.gameOver = dinoBot.gameEnd;
+            confirmReload(true);
+        }
+    }, 0);
+    setTimeout(function(){
+        Runner.instance_.gameEnd();
+    }, solvedTime);
+} else confirmReload(false);
